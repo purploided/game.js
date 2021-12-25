ef446444fc3fad47d1699c17bf1fd8bd291b1ba8
nic <93119331+NicohIas@users.noreply.github.com>
2021-12-25 12:42:18 +1000

    Add files via upload

---
+++ Controllers/random-summoner.js
@@ -0,0 +1,7 @@
+var rollingItems = ['https://cdn.discordapp.com/attachments/920952514323546115/924009934641840168/ezgif-2-17ab284b2e.gif', 'https://cdn.discordapp.com/attachments/920952514323546115/924121490083938344/ezgif-7-71b242d01b.gif']
+
+window.onload = function () {
+	var blockRefresh = document.getElementById("block");
+	var spot = Math.floor(Math.random() * rollingItems.length);
+	blockRefresh.style.background = "url(" + rollingItems[spot] + ")";
+}
